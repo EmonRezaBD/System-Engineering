0bc5ae0852106c8088de9979cc242f5f7204d439
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-09-20 20:23:48 +0600

    First shell program

---
+++ hello.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+echo "Hello World"
+#It is a comment
+
+#This is vars
+name=Reza
+echo My name is $name
+
+echo our home directory is $HOME
+
+firstVal=10
+secval=20
+
+echo $firstVal
+
+echo "Enter name: "
+read yourName
+echo Your name is $yourName
+
+read -p 'username:' user_var #This will allow to give inpuit in the same line
+echo username: $user_var
+
